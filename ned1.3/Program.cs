702f575104508037a19563a0a123cf38f349a2f4
Juliadenisovaaa <denisovajyl@gmail.com>
2025-04-14 10:12:24 +0200

    Добавьте файлы проекта.

---
+++ ned1.3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ned1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0; // счетчик отрицательных чисел
+            string input; // введенная строка пользователем
+            int number; // преобразованный в число ввод пользователя
+            bool isValidInput; // проверка на допустимое значения
+
+            Console.WriteLine("Введите числа (0 для завершения):");q
+
+            do
+            {
+                do
+                {
+                    Console.Write("Введите число: ");
+                    input = Console.ReadLine();
+
+                    isValidInput = int.TryParse(input, out number);
+
+                    if (!isValidInput)
+                    {
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+                    }
+                } while (!isValidInput);
+
+                if (number < 0) // если отрицательное число то увеличиваем
+                {
+                    count++;
+                }
+
+            } while (number != 0);
+
+            Console.WriteLine("Количество отрицательных чисел: " + count);
+            Console.ReadKey();
+
+        }
+    }
+}
